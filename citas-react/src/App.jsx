6d876c6abeabe5cc9b42dc01aa9a5d2d825cfad4
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-09-17 11:06:03 -0600

    Initial commit

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,72 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+import Login from './components/Login'
+import ComboBoxAutocomplete from './components/ComboBoxAutocomplete'
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Form/>
+
+      <br/>
+      <br/>
+
+      <Login/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <span>asdf</span>
+    </div>
+  )
+}
+
+export default App
