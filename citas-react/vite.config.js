3f22d87dc1a6b7d1f50a4e20c8d2a846c93203fa
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-10-06 10:13:34 -0600

    modificacion de importacion

--- citas-react/vite.config.js
+++ citas-react/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 import { resolve } from 'path'
 
 
@@ -12,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src/'),
     },
   },
 })
